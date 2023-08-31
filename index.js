f64f5e4575297ed5f743016e5683333a5cb20ff3
Mert Salik <53333294+codermert@users.noreply.github.com>
2023-08-31 22:56:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const https = require('https');
+const fs = require('fs');
 
 const url = 'https://www.teknosa.com/kampanyalar';
 const agent = new https.Agent({
@@ -17,22 +18,29 @@ axios.get(url, { headers, httpsAgent: agent })
   const promotions = [];
 
   $('.oppurtunities-tab').each((index, element) => {
-    const img = $(element).find('.bnr-media img').attr('src');
-    const product = $(element).find('.bnr-title').text();
+    const resim = $(element).find('.bnr-media img').attr('src');
+    const urun = $(element).find('.bnr-title').text();
     const link = 'https://www.teknosa.com' + $(element).find('.campaign-link').attr('href');
-    const date = $(element).find('.campaign-discount-time').text();
+    const tarih = $(element).find('.campaign-discount-time').text();
 
-    if (img && product && link && date) {
+    if (resim && urun && link && tarih) {
       promotions.push({
-        img,
-        product,
+        resim,
+        urun,
         link,
-        date
+        tarih
       });
     }
   });
 
-  console.log(promotions);
+  // Verileri JSON dosyasına kaydet
+  fs.writeFile('veri.json', JSON.stringify(promotions, null, 2), (err) => {
+    if (err) {
+      console.error('Hata:', err);
+    } else {
+      console.log('Veriler kaydedildi!');
+    }
+  });
 })
 .catch(error => {
   console.error('Hata:', error);
