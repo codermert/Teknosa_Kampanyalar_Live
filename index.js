3f2cf1e583a252800a6b8fee15e91ba5dbb310df
Mert Salık <53333294+codermert@users.noreply.github.com>
2024-07-22 15:10:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const https = require('https');
 const fs = require('fs');
 
 const url = 'https://www.teknosa.com/kampanyalar';
-const agent = new https.Agent({
-  rejectUnauthorized: false
-});
 
-const headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-};
+axios.get(url)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const campaigns = [];
 
-axios.get(url, { headers, httpsAgent: agent })
-.then(response => {
-  const $ = cheerio.load(response.data);
-  const promotions = [];
+    $('div.0-category-0').each((index, element) => {
+      const urun = $(element).find('div.criv-title-inner').text().trim();
+      const resim = $(element).find('img.lazy').attr('data-srcset');
+      const tarih = $(element).find('div.cri-info b').text().trim();
+      const link = $(element).find('a.criv-inner').attr('href');
 
-  $('.oppurtunities-tab').each((index, element) => {
-    const resim = $(element).find('.bnr-media img').attr('src');
-    const urun = $(element).find('.bnr-title').text();
-    const link = 'https://www.teknosa.com' + $(element).find('.campaign-link').attr('href');
-    const tarih = $(element).find('.campaign-discount-time').text();
-
-    if (resim && urun && link && tarih) {
-      promotions.push({
-        resim,
+      campaigns.push({
         urun,
-        link,
-        tarih
+        resim,
+        tarih,
+        link: `https://www.teknosa.com${link}`
       });
-    }
-  });
+    });
 
-  // Verileri JSON dosyasına kaydet
-  fs.writeFile('veri.json', JSON.stringify(promotions, null, 2), (err) => {
-    if (err) {
-      console.error('Hata:', err);
-    } else {
-      console.log('Veriler kaydedildi!');
-    }
+    // Save the data to a JSON file
+    fs.writeFile('veri.json', JSON.stringify(campaigns, null, 2), (err) => {
+      if (err) {
+        console.error('Hata:', err);
+        return;
+      }
+      console.log('Veriler veri.json dosyasına kaydedildi.');
+    });
+  })
+  .catch(error => {
+    console.error('Hata:', error);
   });
-})
-.catch(error => {
-  console.error('Hata:', error);
-});
